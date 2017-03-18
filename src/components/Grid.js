f858168deedd078c778a9b43ea762dce67aa762a
RC <rc.chandru@gmail.com>
2017-03-19 01:36:46 +0530

    Added logic to deduct adjacent tiles selection

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -2,8 +2,65 @@ import React, {Component} from 'react'
 import {GridDiv} from '../styles/Grid'
 import {data} from './data'
 import Tile from './Tile'
+import update from 'immutability-helper';
 
 export default class Grid extends Component {
+
+  state = {currentSelection: []}
+
+  sameRow = (selection, row) => {
+    return selection.every((s) => s.row === row)
+  }
+
+  sameCol = (selection, col) => {
+    return selection.every((s) => s.col === col)
+  }
+
+  areAdjacentTiles = (selection) => {
+    for (let i = 0; i < selection.length - 1; i++) {
+      if (Math.abs(selection[i] - selection[i + 1]) !== 1) {
+        return false
+      }
+    }
+    return true
+  }
+
+  adjacentColumn = (selection, newCol) => {
+    const selectedCols = selection.map((s) => s.col)
+    selectedCols.push(newCol)
+    return this.areAdjacentTiles(selectedCols)
+  }
+
+  adjacentRow = (selection, newRow) => {
+    const selectedRows = selection.map((s) => s.row)
+    selectedRows.push(newRow)
+    return this.areAdjacentTiles(selectedRows)
+  }
+
+  handleSelect = (row, col) => {
+    const {currentSelection} = this.state
+    const firstSelection = currentSelection.length === 0
+
+    // if (!firstSelection) {
+    //   console.log(this.sameRow(currentSelection, row))
+    //   console.log(this.adjacentColumn(currentSelection, col))
+    //   console.log(this.sameCol(currentSelection, col))
+    //   console.log(this.adjacentRow(currentSelection, row))
+    // }
+
+    const adjacentTileSelected = (this.sameRow(currentSelection, row) && this.adjacentColumn(currentSelection, col)) ||
+      (this.sameCol(currentSelection, col) && this.adjacentRow(currentSelection, row))
+
+    if (firstSelection || adjacentTileSelected) {
+      this.setState(update(this.state, {
+        currentSelection: {
+          $push: [{...{row, col}}]
+        }
+      }))
+    } else {
+      this.setState({currentSelection: [{row, col}]})
+    }
+  }
 
   render() {
     return <GridDiv>
@@ -11,11 +68,15 @@ export default class Grid extends Component {
         data.map((row, i) => {
           return row.line.split('').map((letter, j) => {
             const id = `${i}${j}`
+            const selected = this.state.currentSelection.some((sel) => sel.row === i && sel.col === j)
             return <Tile id={id}
                          key={id}
                          letter={letter}
+                         selected={selected}
                          row={i}
-                         col={i}/>
+                         col={j}
+                         notifySelect={this.handleSelect}
+            />
           })
         })
       }
--- src/components/Tile.js
+++ src/components/Tile.js
@@ -3,15 +3,13 @@ import {TileDiv} from '../styles/Grid'
 
 export default class Tile extends Component {
 
-  state = {selected: false}
-
   onSelect = () => {
-    this.setState({selected: true})
+    this.props.notifySelect(this.props.row, this.props.col)
   }
 
   render() {
     const {letter} = this.props
-    const style = this.state.selected ? {background: '#ff4500'} : {}
+    const style = this.props.selected ? {background: '#ff4500'} : {}
     return <TileDiv key={this.props.id} data-letter={letter} style={style} onClick={this.onSelect}/>
   }
 }
@@ -20,5 +18,7 @@ Tile.PropTypes = {
   letter: PropTypes.string.isRequired,
   id: PropTypes.string.isRequired,
   row: PropTypes.number.isRequired,
-  col: PropTypes.number.isRequired
+  col: PropTypes.number.isRequired,
+  selected: PropTypes.bool.isRequired,
+  notifySelect: PropTypes.func
 }
